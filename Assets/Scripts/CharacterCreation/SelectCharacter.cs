75d756c0112377dc8f68aa8ce454d284c734d39f
gabrielpotamianos <33257116+gabrielpotamianos@users.noreply.github.com>
2020-03-05 20:55:23 +0000

    Character Creation Level Setup + Backup

--- Assets/Scripts/CharacterCreation/SelectCharacter.cs
+++ Assets/Scripts/CharacterCreation/SelectCharacter.cs
@@ -8,14 +8,15 @@ public class SelectCharacter : MonoBehaviour
     static public string SelectedGameObject;
     bool rotateLeft = false;
     bool rotateRight = false;
+    public float RotationSpeed=2;
 
     void Update()
     {
         SelectedGameObject = gameObject.tag;
         if (rotateLeft)
-            transform.Rotate(new Vector3(0, 1, 0));
+            transform.Rotate(new Vector3(0, RotationSpeed, 0));
         else if (rotateRight)
-            transform.Rotate(new Vector3(0, -1, 0));
+            transform.Rotate(new Vector3(0, -RotationSpeed, 0));
     }
 
     public void RotateObjectLeft()
